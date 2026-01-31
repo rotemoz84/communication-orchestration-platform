7798598afd1b685e9102757ad26beb40e772b8a4
Shachar Oz <shachar.oz@gmail.com>
2026-01-31 16:55:13 +0200

    Update Footer.tsx

--- site_clinic/src/components/Footer.tsx
+++ site_clinic/src/components/Footer.tsx
@@ -29,7 +29,7 @@ const Footer = () => {
                                 </div>
                                 <div className="flex items-center gap-3 text-slate-500 font-medium">
                                     <Phone className="w-4 h-4 text-secondary-teal" />
-                                    <span className="text-sm font-bold text-primary-navy">04-6801553</span>
+                                    <span className="text-sm font-bold text-primary-navy">04-6801552</span>
                                 </div>
                                 <a
                                     href="https://wa.me/+972506993353?text=%D7%A9%D7%9C%D7%95%D7%9D%2C%20%D7%90%D7%A0%D7%99%20%D7%9E%D7%A2%D7%95%D7%A0%D7%99%D7%99%D7%A0%D7%AA%20%D7%9C%D7%A7%D7%91%D7%9C%20%D7%A2%D7%95%D7%93%20%D7%A4%D7%A8%D7%98%D7%99%D7%9D%20%D7%A2%D7%9C%20%D7%94%D7%A9%D7%99%D7%A8%D7%95%D7%AA%D7%99%D7%9D%20%D7%A9%D7%9C%20%D7%94%D7%9E%D7%A8%D7%A4%D7%90%D7%94"
