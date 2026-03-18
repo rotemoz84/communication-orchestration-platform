e57efef227fa7ba774990bdc1838f8f00f48a054
Shachar Oz <shachar.oz@gmail.com>
2026-03-18 12:57:56 +0100

    update phone of whatsapp

--- site_clinic/src/components/Footer.tsx
+++ site_clinic/src/components/Footer.tsx
@@ -34,7 +34,7 @@ const Footer = () => {
                             buttonColor="#25D366"
                             buttonText="שילחי לי וואטסאפ"
                             buttonIcon={<img src={whatsappLogo} alt="WhatsApp" className="generic-button-icon" />}
-                            href="https://wa.me/+972505449402?text=%D7%A9%D7%9C%D7%95%D7%9D%2C%20%D7%90%D7%A0%D7%99%20%D7%9E%D7%A2%D7%95%D7%A0%D7%99%D7%99%D7%A0%D7%AA%20%D7%9C%D7%A7%D7%91%D7%9C%20%D7%A2%D7%95%D7%93%20%D7%A4%D7%A8%D7%98%D7%99%D7%9D%20%D7%A2%D7%9C%20%D7%94%D7%A9%D7%99%D7%A8%D7%95%D7%AA%D7%99%D7%9D%20%D7%A9%D7%9C%20%D7%94%D7%9E%D7%A8%D7%A4%D7%90%D7%94"
+                            href="https://wa.me/+972509996171?text=%D7%A9%D7%9C%D7%95%D7%9D%2C%20%D7%90%D7%A0%D7%99%20%D7%9E%D7%A2%D7%95%D7%A0%D7%99%D7%99%D7%A0%D7%AA%20%D7%9C%D7%A7%D7%91%D7%9C%20%D7%A2%D7%95%D7%93%20%D7%A4%D7%A8%D7%98%D7%99%D7%9D%20%D7%A2%D7%9C%20%D7%94%D7%A9%D7%99%D7%A8%D7%95%D7%AA%D7%99%D7%9D%20%D7%A9%D7%9C%20%D7%94%D7%9E%D7%A8%D7%A4%D7%90%D7%94"
                             target="_blank"
                         />
                     </div>
